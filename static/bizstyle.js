62454dc934958467f78546495519b11ae9001e00
Kyle Knoepfel <knoepfel@fnal.gov>
2025-07-07 13:37:38 -0500

    Pin at v0.1 documentation

--- static/bizstyle.js
+++ static/bizstyle.js
@@ -17,7 +17,7 @@ const initialiseBizStyle = () => {
 }
 
 window.addEventListener("resize",
-  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Phlex 0.2.alpha (f93facb) documentation")
+  () => (document.querySelector("li.nav-item-0 a").innerText = (window.innerWidth <= 776) ? "Top" : "Phlex 0.1 documentation")
 )
 
 if (document.readyState !== "loading") initialiseBizStyle()
--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.2.alpha (f93facb)',
+    VERSION: '0.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
